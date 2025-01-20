0b4d4655c7c5ca0c82049be6348472bb7fc25569
AnnaRoumelioti <mrs.adelaida@gmail.com>
2025-01-20 20:00:54 +0200

    updated lessons

--- src/gr/aueb/cf/ch14/Main.java
+++ src/gr/aueb/cf/ch14/Main.java
@@ -6,23 +6,24 @@ import gr.aueb.cf.ch14.services.CodingFactorySingleton;
 import gr.aueb.cf.ch14.services.HelloUtil;
 
 public class Main {
-    Teacher teacher1 = new Teacher(1, "Ath.", "Androutsos");
-    Teacher teacher2 = new Teacher(2, "A.", "Giannoutsou");
-    Teacher teacher3 = new Teacher(3, "C.", "Papadopoulos");
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher(1, "Ath.", "Androutsos");
+        Teacher teacher2 = new Teacher(2, "A.", "Giannoutsou");
+        Teacher teacher3 = new Teacher(3, "C.", "Papadopoulos");
 
         HelloUtil.sayHello();
 
-    Point p1 = Point.getInstance();
-    Point p2 = Point.getRandomPoint();
+        Point p1 = Point.getInstance();
+        Point p2 = Point.getRandomPoint();
 
         System.out.println(p1);
         System.out.println(p2);
 
-    CodingFactorySingleton cf1;
-    cf1 = CodingFactorySingleton.getInstance();
-    CodingFactorySingleton cf2 = CodingFactorySingleton.getInstance();
+        CodingFactorySingleton cf1;
+        cf1 = CodingFactorySingleton.getInstance();
+        CodingFactorySingleton cf2 = CodingFactorySingleton.getInstance();
 
         System.out.println(cf1);
         System.out.println(cf2);
-}
+    }
 }
