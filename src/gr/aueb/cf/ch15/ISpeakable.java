f8e119e051a3f97f669a2a8f9d9f437a7f17f5df
AnnaRoumelioti <mrs.adelaida@gmail.com>
2025-01-21 08:02:30 +0200

    new

---
+++ src/gr/aueb/cf/ch15/ISpeakable.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch15;
+
+public interface ISpeakable {
+    /**
+     * Makes a sound like a speech.
+     * This method is public and abstract (by default).
+     */
+    void speak();
+}
